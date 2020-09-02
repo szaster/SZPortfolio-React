27b4560e6f02c8352a0e36a7b6c6938d6fbb8ff1
Svitlana Zaster <szaster@protonmail.com>
2020-09-02 17:53:44 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import MyNavbar from "./components/navbar/mynavbar";
 
 import "./App.css";
 
 function App() {
-  return <div className="App">My portfolio</div>;
+  return (
+    <div className="App">
+      My portfolio
+      <MyNavbar />
+    </div>
+  );
 }
 
 export default App;
