6dff14bc4ade6e6fbfb0608cd33cc1a9d2791db1
Svitlana Zaster <szaster@protonmail.com>
2020-09-02 23:43:25 -0500

    Updates

---
+++ precache-manifest.c8444ea56e03519d56d33db349e968a6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8724416336519fd720ea742ccdc27a9",
+    "url": "/SZPortfolio-React/index.html"
+  },
+  {
+    "revision": "1059e1db175809c432de",
+    "url": "/SZPortfolio-React/static/css/main.e10f0c84.chunk.css"
+  },
+  {
+    "revision": "07306116771fbfcdc6e3",
+    "url": "/SZPortfolio-React/static/js/2.2b8df52b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/SZPortfolio-React/static/js/2.2b8df52b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1059e1db175809c432de",
+    "url": "/SZPortfolio-React/static/js/main.bc8ccb5d.chunk.js"
+  },
+  {
+    "revision": "9f9f117b713d5b837756",
+    "url": "/SZPortfolio-React/static/js/runtime-main.11b41c2c.js"
+  },
+  {
+    "revision": "d659a1762237e3da84c261f6bafac497",
+    "url": "/SZPortfolio-React/static/media/logo.d659a176.png"
+  },
+  {
+    "revision": "90c8e71cb39b049ef03f38f26d0f781c",
+    "url": "/SZPortfolio-React/static/media/profile.90c8e71c.jpeg"
+  },
+  {
+    "revision": "10be0220480ea288e1de763e0d8df19a",
+    "url": "/SZPortfolio-React/static/media/slide1.10be0220.jpeg"
+  },
+  {
+    "revision": "1a0338920b1e50b6456f0395be251221",
+    "url": "/SZPortfolio-React/static/media/slide2.1a033892.jpg"
+  },
+  {
+    "revision": "644aa6ba17fadda9eb08c55ae4203d6c",
+    "url": "/SZPortfolio-React/static/media/slide3.644aa6ba.jpg"
+  }
+]);
