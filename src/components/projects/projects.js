63e61b6e7b3ad81bff7fcebff9eddfcbcef4d88b
Svitlana Zaster <szaster@protonmail.com>
2020-12-17 21:22:46 -0600

    clean up

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Timeline, Events, UrlButton, ImageEvent } from "@merc/react-timeline";
-
+import "./style.css";
 import MiddleGround from "../../assets/img/projects/mdlgrnd.png";
 import IdeaExpo from "../../assets/img/projects/idea-expo.png";
 import EmployeeDirectory from "../../assets/img/projects/employee-directory.png";
@@ -21,11 +21,11 @@ const TimeLine = () => {
       <Timeline>
         <Events>
           <ImageEvent
-            // date="05/29/2020"
+            date="09/29/2020"
             className="text-center"
-            text="Your Breaking News"
-            src={MiddleGround}
-            alt="Middle ground"
+            text="Letflex or Netflix?"
+            src={Letflex}
+            alt="Letflex"
           >
             <div className="d-flex justify-content-between flex-column mt-1">
               <div>
@@ -41,33 +41,33 @@ const TimeLine = () => {
 
                     <Accordion.Collapse eventKey="0" className="text-left">
                       <Card.Body>
-                        <strong>Description:</strong>This project aims to bring
-                        you news in an objective fashion from the whole
-                        political spectrum.
+                        <strong>Description: </strong> Letflex is Netflix clone
+                        app featuring movie search name or keywords. Users can
+                        watch trailers, add movies to favorites, comment,
+                        review, and discuss movies. Letflex has a blog/dashboard
+                        feature with recommendations and movie reviews.
                         <hr />
-                        <strong>Features:</strong>
-                        <ul className="list-styles pt-1">
-                          <li>Search 3 news sources</li>
-                          <li>
-                            Real-time API calls to NY Times, Bing news, and
-                            Hoaxy
-                          </li>
-                        </ul>
+                        <strong>Role: </strong> Mostly worked on back-end,
+                        authorization flow, and on the connection with
+                        front-end. Created data models for MongoDB Atlas
+                        database, wrote Express route controllers, configured
+                        Passport, and implemented user sign-in authorization
+                        with Google OAuth.
                         <hr />
+                        <strong>Tools/languages: </strong>JavaScript, React,
+                        Redux, Express, Passport, Mongoose, Node, Heroku,
+                        MongoDB Atlas.
                       </Card.Body>
                     </Accordion.Collapse>
                   </Card>
                 </Accordion>
               </div>
               <div className="d-flex justify-content-between flex-nowrap text-center">
-                <UrlButton
-                  href=" https://szaster.github.io/Your-Breaking-News/"
-                  target="_blank"
-                >
+                <UrlButton href="http://letflex.herokuapp.com/" target="_blank">
                   SEE LIVE
                 </UrlButton>
                 <UrlButton
-                  href=" https://github.com/szaster/Your-Breaking-News"
+                  href="https://github.com/szaster/Letflex"
                   target="_blank"
                 >
                   SOURCE CODE
@@ -77,11 +77,11 @@ const TimeLine = () => {
           </ImageEvent>
 
           <ImageEvent
-            // date="05/29/2020"
+            date="07/01/2020"
             className="text-center"
-            text="Idea Expo"
+            text="IdeaExpo"
             src={IdeaExpo}
-            alt="Ideaexpo"
+            alt="IdeaExpo"
           >
             <div className="d-flex justify-content-between flex-column mt-1">
               <div>
@@ -97,16 +97,18 @@ const TimeLine = () => {
 
                     <Accordion.Collapse eventKey="0" className="text-left">
                       <Card.Body>
-                        <strong>Description:</strong>Storage for project ideas
-                        to be shared, discussed, exchanged, developed.
+                        <strong>Description: </strong> Web application allows to
+                        search, post, develop business/project ideas, and find
+                        collaborators on the ideas of interest.
                         <hr />
-                        <strong>Features:</strong>
-                        <ul className="list-styles pt-1">
-                          <li>Search, post, edit, and read business ideas</li>
-                          <li>
-                            Collaborate on the applications of your interest
-                          </li>
-                        </ul>
+                        <strong>Role: </strong>Created models for MySQL
+                        database, wrote Express route controllers, configured
+                        Passport, implemented user sign-in authorization with
+                        Google OAuth.
+                        <hr />
+                        <strong>Tools/languages: </strong> HTML, JavaScript,
+                        CSS, Materialize CSS, MySQL, Sequelize, Express, Node,
+                        Handlebars, Heroku.
                         <hr />
                       </Card.Body>
                     </Accordion.Collapse>
@@ -131,7 +133,7 @@ const TimeLine = () => {
           </ImageEvent>
 
           <ImageEvent
-            // date="05/29/2020"
+            date="08/29/2020"
             className="text-center"
             text="Employee Directory"
             src={EmployeeDirectory}
@@ -151,18 +153,14 @@ const TimeLine = () => {
 
                     <Accordion.Collapse eventKey="0" className="text-left">
                       <Card.Body>
-                        <strong>Description:</strong>This project displays a
-                        list of random users(employees) on every refresh and
-                        allows view and edit the entire employee directory.
+                        <strong>Description: </strong>This app displays a list
+                        of random users(employees) on every refresh and allows
+                        to view and edit the entire employee directory. The user
+                        can search for a specific name or sort names in
+                        ascending or descending order, or by DOB.
                         <hr />
-                        <strong>Features:</strong>
-                        <ul className="list-styles pt-1">
-                          <li>
-                            View, search, sort the employee directory by name
-                            and DOB.
-                          </li>
-                        </ul>
-                        <hr />
+                        <strong>Tools/languages: </strong>JavaScript, React,
+                        CSS.
                       </Card.Body>
                     </Accordion.Collapse>
                   </Card>
@@ -186,7 +184,7 @@ const TimeLine = () => {
           </ImageEvent>
 
           <ImageEvent
-            // date="05/29/2020"
+            date="06/29/2020"
             className="text-center"
             text="Burger"
             src={Burger}
@@ -206,18 +204,12 @@ const TimeLine = () => {
 
                     <Accordion.Collapse eventKey="0" className="text-left">
                       <Card.Body>
-                        <strong>Description:</strong>This application records
-                        your most delicious burgers into a very special top
-                        burger list!
+                        <strong>Description: </strong>This application allows to
+                        keep records of user's most delicious burgers in the
+                        very special burger list!
                         <hr />
-                        <strong>Features:</strong>
-                        <ul className="list-styles pt-1">
-                          <li>Record and edit your burger list.</li>
-                          <li>
-                            This app is written with MySQL, Node, Express,
-                            Handlebars and ORM.
-                          </li>
-                        </ul>
+                        <strong>Tools/languages: </strong>MySQL, Node, Express,
+                        Handlebars and ORM.
                         <hr />
                       </Card.Body>
                     </Accordion.Collapse>
@@ -242,11 +234,11 @@ const TimeLine = () => {
           </ImageEvent>
 
           <ImageEvent
-            // date="05/29/2020"
+            date="05/29/2020"
             className="text-center"
-            text="Letflex or Netflix?"
-            src={Letflex}
-            alt="Letlex"
+            text="Your Breaking News"
+            src={MiddleGround}
+            alt="Middle ground"
           >
             <div className="d-flex justify-content-between flex-column mt-1">
               <div>
@@ -262,17 +254,17 @@ const TimeLine = () => {
 
                     <Accordion.Collapse eventKey="0" className="text-left">
                       <Card.Body>
-                        <strong>Description:</strong>
+                        <strong>Description: </strong> Web application that
+                        aggregates articles from various news sources aiming to
+                        deliver unbiased information to readers. Real-time API
+                        calls to NY Times, Bing news, and Hoaxy.
                         <hr />
-                        <strong>Features:</strong>
-                        <ul className="list-styles pt-1">
-                          <li>Login with Google!</li>
-                          <li>
-                            Search and watch movie trailers, add to favorites,
-                            leave comments and discuss movies and shows!
-                          </li>
-                          <li>This app is written using React.</li>
-                        </ul>
+                        <strong>Role: </strong> Designed and implemented user
+                        interface, worked on functionality, and business logic
+                        of the app.
+                        <hr />
+                        <strong>Tools/languages: </strong>HTML, JavaScript,
+                        jQuery, CSS, Materialize CSS.
                         <hr />
                       </Card.Body>
                     </Accordion.Collapse>
@@ -280,11 +272,14 @@ const TimeLine = () => {
                 </Accordion>
               </div>
               <div className="d-flex justify-content-between flex-nowrap text-center">
-                <UrlButton href="http://letflex.herokuapp.com/" target="_blank">
+                <UrlButton
+                  href=" https://szaster.github.io/Your-Breaking-News/"
+                  target="_blank"
+                >
                   SEE LIVE
                 </UrlButton>
                 <UrlButton
-                  href="https://github.com/szaster/Letflex"
+                  href=" https://github.com/szaster/Your-Breaking-News"
                   target="_blank"
                 >
                   SOURCE CODE
